f63f7237cc42a33680a18847af423b03ddd9de50
xtsumit <mahadik.sumit97@gmail.com>
2024-03-20 23:11:46 +0530

    Add project files.

---
+++ NZWalks.Api/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalks.Api.Models.Domain
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+
+        //Navigation Property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; }
+    }
+}
