581a84beadc47ab82f5d66166d2dd60854e986a7
xtsumit <mahadik.sumit97@gmail.com>
2024-03-28 17:06:32 +0530

    Login and Register Domain Models.

---
+++ NZWalks.Api/Models/DTO/loginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.Models.DTO
+{
+    public class loginRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
